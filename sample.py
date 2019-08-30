1d6e2dafa5913396e79c659a61044123b332d098
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-08-30 15:04:25 +0900

    import order fix

--- sample.py
+++ sample.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
+from datetime                      import datetime
 from selenium                      import webdriver
 from selenium.webdriver.common.by  import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support    import expected_conditions as ec
-from datetime                      import datetime
+
 import logging
 import re
 import time
